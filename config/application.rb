444d992e1c589f113a9eba0d461a86794273a315
sri-m <srinivaspkl@gmail.com>
2014-10-05 08:29:35 +0530

    Added Configuration

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Ramchandra
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.active_record.observers = :contact_observer
+    config.assets.initialize_on_precompile = false
   end
 end
